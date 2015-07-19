724fc34e55b9bfbd7efe98e1fe029155a612b6a3
justin8d <jshirley@me.com>
2015-07-19 15:22:28 -0400

    Adds Direct Message and Follower Spam DM. Also allows any sort of capitalization for user names.

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -10,6 +10,20 @@ class MicroBlogger
 
 	def run
 		puts "Welcome to the JSl Twitter Client"
+		command = ""
+		while command != "q"
+			printf "Enter command: "
+			input = gets.chomp
+			parts = input.split(" ")
+			command = parts[0]
+			case command
+				when 'q' then puts 'Goodbye!'
+				when 't' then tweet(parts[1..-1].join(" "))
+				when 'dm' then dm(parts[1], parts[2..-1].join(" "))
+				when 'spam' then spam_my_followers(parts[1..-1].join" ")
+				else puts "Sorry, '#{command}' is not a valid command."
+			end
+		end
 	end
 
 	def tweet(message)
@@ -20,7 +34,31 @@ class MicroBlogger
 		end
 	end
 
+	def dm(target, message)
+		puts "Sending this Direct Message to #{target}: "
+		puts message
+		screen_names = 	@client.followers.collect {|follower| @client.user(follower).screen_name}
+		screen_names.map!(&:downcase)
+		target.downcase!
+		if screen_names.include?(target)
+			message = "d @#{target} #{message}"
+			tweet(message)
+		else
+			puts "Sorry. You may only send Direct Messages to your followers."
+		end
+	end
+
+	def followers_list
+    	screen_names = []
+    	@client.followers.each {|follower| screen_names << @client.user(follower).screen_name}
+    	screen_names.map!(&:downcase)
+  	end
+
+  	def spam_my_followers(message)
+    	followers_list.each { |follower| dm(follower, message)}
+  	end
+
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("".ljust(140, "test"))
+blogger.run
